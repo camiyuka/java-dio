099a33fbb14be3a6c5364c57002f0776a1843b27
camiyuka <camila.yatabe@gmail.com>
2023-03-21 15:51:01 -0300

    tipos-variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
+
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+       boolean condicao1= true;
+       boolean condicao2= false;
+
+       if(condicao1 && (6>2)){
+        System.out.println("as duas condições são verdadeiras");
+       }
+
+       if(condicao1 || condicao2){
+        System.out.println("uma das condições é verdadeira ");
+       }
     }
 }
