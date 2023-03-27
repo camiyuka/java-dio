83e1b4b7a4acebfd6180f2effbd249e6db4ef785
camiyuka <camila.yatabe@gmail.com>
2023-03-27 14:54:49 -0300

    InputMismatchException

--- Java-Terminal/src/AboutMe.java
+++ Java-Terminal/src/AboutMe.java
@@ -1,15 +1,34 @@
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
+    public static void main(String[] args) {
+        try{
+        //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+        
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
 
-    public static void main (String[] args){
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+        
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
 
-        String nome = args [0];
-        String sobrenome = args [1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
+        
+        //imprimindo os dados obtidos pelo usuario
+        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+        }
+        catch(InputMismatchException excecao){
+            System.err.println("os campos idade e altura precisam ser numéricos");
 
-        System.out.println("olá, me chamo" +  " "+ nome+ " " + sobrenome );
-        System.out.println("tenho" +  " " + idade +  " " + "anos" );
-        System.out.println("minha altura é"  +  " "+ altura +  " " +"cm");
+        }
     }
-    
 }
--- Java-Terminal/src/Calculadora.java
+++ Java-Terminal/src/Calculadora.java
@@ -7,6 +7,8 @@ public class Calculadora {
    */
   public int somar(int numeroUm, int numeroDois) {
     return  numeroUm + numeroDois;
+    
+    
 }
 
 }
